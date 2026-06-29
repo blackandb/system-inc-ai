955e053ae14a434d3be4847bab3eca8ddbaea38a
blackandb <xeurxeur1@gmail.com>
2026-06-30 02:49:26 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import { Background } from "@/components/layout/background";
 import { Navbar } from "@/components/layout/navbar";
-
 import { Hero } from "@/components/features/landing/hero";
-import { CompanyFactory } from "@/components/features/landing/company-factory";
+import { Footer } from "@/components/layout/footer";
 
 export default function HomePage() {
   return (
     <Background>
       <Navbar />
       <Hero />
-      <CompanyFactory />
+      <Footer />
     </Background>
   );
 }
