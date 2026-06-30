1f60d99ed9265b6dca911ce95fa4707ea4ce8c96
blackandb <xeurxeur1@gmail.com>
2026-06-30 14:53:34 +0300

    Create countries.ts

---
+++ lib/countries.ts
@@ -0,0 +1,243 @@
+export type Country = {
+  code: string;
+  name: string;
+  currency: string;
+  language: string;
+};
+
+export const countries: Country[] = [
+  {
+    code: "RO",
+    name: "Romania",
+    currency: "RON",
+    language: "Romanian",
+  },
+  {
+    code: "US",
+    name: "United States",
+    currency: "USD",
+    language: "English",
+  },
+  {
+    code: "GB",
+    name: "United Kingdom",
+    currency: "GBP",
+    language: "English",
+  },
+  {
+    code: "DE",
+    name: "Germany",
+    currency: "EUR",
+    language: "German",
+  },
+  {
+    code: "FR",
+    name: "France",
+    currency: "EUR",
+    language: "French",
+  },
+  {
+    code: "IT",
+    name: "Italy",
+    currency: "EUR",
+    language: "Italian",
+  },
+  {
+    code: "ES",
+    name: "Spain",
+    currency: "EUR",
+    language: "Spanish",
+  },
+  {
+    code: "NL",
+    name: "Netherlands",
+    currency: "EUR",
+    language: "Dutch",
+  },
+  {
+    code: "BE",
+    name: "Belgium",
+    currency: "EUR",
+    language: "Dutch / French",
+  },
+  {
+    code: "LU",
+    name: "Luxembourg",
+    currency: "EUR",
+    language: "Luxembourgish",
+  },
+  {
+    code: "PL",
+    name: "Poland",
+    currency: "PLN",
+    language: "Polish",
+  },
+  {
+    code: "CZ",
+    name: "Czech Republic",
+    currency: "CZK",
+    language: "Czech",
+  },
+  {
+    code: "AT",
+    name: "Austria",
+    currency: "EUR",
+    language: "German",
+  },
+  {
+    code: "CH",
+    name: "Switzerland",
+    currency: "CHF",
+    language: "German / French",
+  },
+  {
+    code: "SE",
+    name: "Sweden",
+    currency: "SEK",
+    language: "Swedish",
+  },
+  {
+    code: "NO",
+    name: "Norway",
+    currency: "NOK",
+    language: "Norwegian",
+  },
+  {
+    code: "DK",
+    name: "Denmark",
+    currency: "DKK",
+    language: "Danish",
+  },
+  {
+    code: "FI",
+    name: "Finland",
+    currency: "EUR",
+    language: "Finnish",
+  },
+  {
+    code: "IE",
+    name: "Ireland",
+    currency: "EUR",
+    language: "English",
+  },
+  {
+    code: "PT",
+    name: "Portugal",
+    currency: "EUR",
+    language: "Portuguese",
+  },
+  {
+    code: "GR",
+    name: "Greece",
+    currency: "EUR",
+    language: "Greek",
+  },
+  {
+    code: "BG",
+    name: "Bulgaria",
+    currency: "BGN",
+    language: "Bulgarian",
+  },
+  {
+    code: "HU",
+    name: "Hungary",
+    currency: "HUF",
+    language: "Hungarian",
+  },
+  {
+    code: "EE",
+    name: "Estonia",
+    currency: "EUR",
+    language: "Estonian",
+  },
+  {
+    code: "LV",
+    name: "Latvia",
+    currency: "EUR",
+    language: "Latvian",
+  },
+  {
+    code: "LT",
+    name: "Lithuania",
+    currency: "EUR",
+    language: "Lithuanian",
+  },
+  {
+    code: "AE",
+    name: "United Arab Emirates",
+    currency: "AED",
+    language: "Arabic",
+  },
+  {
+    code: "TR",
+    name: "Turkey",
+    currency: "TRY",
+    language: "Turkish",
+  },
+  {
+    code: "IL",
+    name: "Israel",
+    currency: "ILS",
+    language: "Hebrew",
+  },
+  {
+    code: "IN",
+    name: "India",
+    currency: "INR",
+    language: "Hindi / English",
+  },
+  {
+    code: "CN",
+    name: "China",
+    currency: "CNY",
+    language: "Chinese",
+  },
+  {
+    code: "JP",
+    name: "Japan",
+    currency: "JPY",
+    language: "Japanese",
+  },
+  {
+    code: "KR",
+    name: "South Korea",
+    currency: "KRW",
+    language: "Korean",
+  },
+  {
+    code: "SG",
+    name: "Singapore",
+    currency: "SGD",
+    language: "English",
+  },
+  {
+    code: "AU",
+    name: "Australia",
+    currency: "AUD",
+    language: "English",
+  },
+  {
+    code: "CA",
+    name: "Canada",
+    currency: "CAD",
+    language: "English / French",
+  },
+  {
+    code: "BR",
+    name: "Brazil",
+    currency: "BRL",
+    language: "Portuguese",
+  },
+  {
+    code: "AR",
+    name: "Argentina",
+    currency: "ARS",
+    language: "Spanish",
+  },
+  {
+    code: "UA",
+    name: "Ukraine",
+    currency: "UAH",
+    language: "Ukrainian",
+  },
+];
